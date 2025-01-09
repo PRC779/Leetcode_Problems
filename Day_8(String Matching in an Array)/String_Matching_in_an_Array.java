ac9c81fcf10ce1861c2834a6a80623ba09dc0769
Paras.R.Chauhan <155594823+PRC779@users.noreply.github.com>
2025-01-09 10:00:24 +0530

    Add files via upload

---
+++ Day_8(String Matching in an Array)/String_Matching_in_an_Array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<String> stringMatching(String[] words) {
+        List<String> list = new ArrayList<>();
+        int len = words.length;
+        for(int i = 0; i < len; i++){
+            for(int j = 0; j < len; j++){
+                if(i == j){
+                    continue;
+                }
+                if(words[j].contains(words[i]) && !list.contains(words[i])){
+                    list.add(words[i]);
+                }
+            }
+        }
+        return list;
+    }
+}
