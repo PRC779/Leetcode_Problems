4e6a2728bbe7c07f7619e8bd6d11896469de6c5e
Paras.R.Chauhan <155594823+PRC779@users.noreply.github.com>
2025-01-26 08:06:13 +0530

    Add files via upload

---
+++ Day_25(Find Eventual Safe States)/Find_Eventual_Safe_States.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        List<Integer> res = new ArrayList<>();
+        if(graph == null || graph.length == 0)  return res;
+        int nodeCountlen = graph.length;
+        int[] color = new int[nodeCountlen];
+        for(int i = 0;i < nodeCountlen;i++){
+            if(dfs(graph, i, color))    res.add(i);
+        }
+        return res;
+    }
+    public boolean dfs(int[][] graph, int start, int[] color){
+        if(color[start] != 0)   return color[start] == 1;
+        color[start] = 2;
+        for(int newNode : graph[start]){
+            if(!dfs(graph, newNode, color))    return false;
+        }
+        color[start] = 1;
+        return true;
+    }
+}
