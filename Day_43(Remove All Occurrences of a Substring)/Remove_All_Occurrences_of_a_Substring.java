669a7b884be93b0927ea3b2cb594046bf9cda632
Paras.R.Chauhan <155594823+PRC779@users.noreply.github.com>
2025-02-13 05:22:50 +0530

    Add files via upload

---
+++ Day_43(Remove All Occurrences of a Substring)/Remove_All_Occurrences_of_a_Substring.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String removeOccurrences(String s, String part) {
+        while(s.contains(part)){
+            int i = s.indexOf(part);
+            int len = part.length();
+            int len2 = s.length();
+            s = s.substring(0,i)+s.substring(len+i,len2);
+        }
+        return s;
+    }
+}
