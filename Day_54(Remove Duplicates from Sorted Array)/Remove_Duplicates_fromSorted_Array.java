6e2e76f7f84e5caf3f961b4b413c55472a10cbb1
Paras.R.Chauhan <155594823+PRC779@users.noreply.github.com>
2025-02-25 05:17:56 +0530

    Add files via upload

---
+++ Day_54(Remove Duplicates from Sorted Array)/Remove_Duplicates_fromSorted_Array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int cntk = 0;
+        int len = nums.length;
+        for(int i=0; i<len; i++){
+            if(nums[i] != nums[cntk]){
+                cntk++;
+                nums[cntk] = nums[i];
+            }
+        }
+        return cntk+1;
+    }
+}
