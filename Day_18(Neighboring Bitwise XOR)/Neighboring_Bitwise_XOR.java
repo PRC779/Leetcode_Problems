bf4bba9e8758702a70a7618a33698bf3ea9b808d
Paras.R.Chauhan <155594823+PRC779@users.noreply.github.com>
2025-03-01 16:56:05 +0530

    Neighboring_Bitwise_XOR.java

--- Day_18(Neighboring Bitwise XOR)/Neighboring_Bitwise_XOR.java
+++ Day_18(Neighboring Bitwise XOR)/Neighboring_Bitwise_XOR.java
@@ -1,5 +1,5 @@
 class Solution {
-    public boolean doesValidArrayExist(int[] derived) {
+    public boolean doesValidArrayExist(int[] derived){
         int cnt = 0;
         int len = derived.length;
         for(int i =0;i<len;i++){
